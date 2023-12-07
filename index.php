1a28a17d61be5843a49d8efc06cb74a5290c62ac
Roy van Empel <roy@wearekonzept.nl>
2023-12-07 20:27:46 +0100

    Add load current day at load

--- index.php
+++ index.php
@@ -236,6 +236,12 @@ $activeDay = $contentLoader->getActiveDay($year);
 
         loadTask(activeSection, activeDay);
     }
+
+    document.addEventListener('DOMContentLoaded', () => {
+        const activeDay = getActiveDay();
+        const activeSection = document.querySelector(`#day-${activeDay}`);
+        loadTask(activeSection, activeDay);
+    });
 </script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
