3a29ca973824a7b9b5c69910ac1206d3e2e4116a
NJM <n.janssen1992@gmail.com>
2023-12-12 18:10:57 +0100

    day 11 second

--- Days/y2023/Day11/Day11Second.php
+++ Days/y2023/Day11/Day11Second.php
@@ -32,25 +32,45 @@ class Day11Second extends Day11First implements \Days\Day
 
         $points = $this->getNumbers($map);
 
-        $this->show($map);
+//        $this->show($map);
 
         return $this->count2($points, $rows, $columns);
     }
 
     public function count2($points, $rows, $columns){
+        $up = 1000000 - 1;
         $total = 0;
         for ($p = 1; $p <= count($points); $p++) {
             for ($k = $p+1; $k <= count($points) ; $k++) {
                 $x = $points[$p][0] - $points[$k][0];
                 if($x < 0){
-
                     $x *= -1;
+                    for ($i = $points[$p][0]; $i < $points[$k][0]; $i++) {
+                        if(in_array($i, $rows)){
+                            $x += $up;
+                        }
+                    }
                 }else{
-
+                    for ($i = $points[$k][0]; $i < $points[$p][0]; $i++) {
+                        if(in_array($i, $rows)){
+                            $x += $up;
+                        }
+                    }
                 }
                 $y = $points[$p][1] - $points[$k][1];
                 if($y < 0){
                     $y *= -1;
+                    for ($i = $points[$p][1]; $i < $points[$k][1]; $i++) {
+                        if(in_array($i, $columns)){
+                            $y += $up;
+                        }
+                    }
+                }else{
+                    for ($i = $points[$k][1]; $i < $points[$p][1]; $i++) {
+                        if(in_array($i, $columns)){
+                            $y += $up;
+                        }
+                    }
                 }
                 $total += $x + $y;
             }
