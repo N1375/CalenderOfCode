a2fa59edd535cfc458d31858976fb295351d11d7
NJM <n.janssen1992@gmail.com>
2023-12-12 18:11:23 +0100

    day 12

---
+++ Days/y2023/Day12/Day12First.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Days\y2023\Day12;
+
+class Day12First implements \Days\Day
+{
+    /**
+     * @var string[]
+     */
+    public array $sequence;
+
+    /**
+     * @var string
+     */
+    public string $pattern;
+
+    public function run(array $input): int|string
+    {
+        if (file_exists('first.txt')){
+            return file_get_contents('first.txt');
+        }
+        if (isset($_GET['test'])) {
+            $input = [
+                '???.### 1,1,3',
+                '.??..??...?##. 1,1,3',
+                '?#?#?#?#?#?#?#? 1,3,1,6',
+                '????.#...#... 4,1,1',
+                '????.######..#####. 1,6,5',
+                '?###???????? 3,2,1',
+            ];
+        }
+
+        $subTotal = 0;
+        foreach ($input as $row) {
+            $pattern = explode(' ', $row);
+            $this->sequence = explode(',', $pattern[1]);
+            $this->pattern = '.' . $pattern[0] . '.';
+            $total = $this->checkPattern(0, 0, 0);
+//            echo $this->pattern . ' ' . implode(',',$this->sequence) ."::".$total."<br/>";
+            $subTotal += $total;
+        }
+
+        return $subTotal;
+    }
+
+    /**
+     * @param int $char
+     * @param int $step
+     * @param int $left
+     *
+     * @return int
+     */
+    public function checkPattern(int $char, int $step, int $left): int
+    {
+        if ($char === strlen($this->pattern)) {
+            if ($step === count($this->sequence) && $left === 0) {
+                return 1;
+            }
+            return 0;
+        }
+        if ($step > count($this->sequence)) {
+            return 0;
+        }
+        $total = 0;
+        if (substr($this->pattern, $char, 1) === '.') {
+            if ($left === 0) {
+                if ($step < count($this->sequence)) {
+                    $total += $this->checkPattern($char + 1, $step + 1, $this->sequence[$step]);
+                }
+                $total += $this->checkPattern($char + 1, $step, 0);
+            }
+        } elseif (substr($this->pattern, $char, 1) === '#') {
+            if ($left > 0) {
+                $total += $this->checkPattern($char + 1, $step, $left - 1);
+            }
+        } else {
+            if ($left === 0) {
+                if ($step < count($this->sequence)) {
+                    $total += $this->checkPattern($char + 1, $step + 1, $this->sequence[$step]);
+                }
+                $total += $this->checkPattern($char + 1, $step, 0);
+            }
+            if ($left > 0) {
+                $total += $this->checkPattern($char + 1, $step, $left - 1);
+            }
+        }
+        return $total;
+    }
+}
---
+++ Days/y2023/Day12/Day12Second.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Days\y2023\Day12;
+
+class Day12Second extends Day12First implements \Days\Day
+{
+    public function run(array $input): int|string
+    {
+        if (file_exists('second.txt')){
+            return file_get_contents('second.txt');
+        }
+        if (isset($_GET['test'])) {
+            $input = [
+                '???.### 1,1,3',
+                '.??..??...?##. 1,1,3',
+                '?#?#?#?#?#?#?#? 1,3,1,6',
+                '????.#...#... 4,1,1',
+                '????.######..#####. 1,6,5',
+                '?###???????? 3,2,1',
+            ];
+        }
+
+        $subTotal = 0;
+        foreach ($input as $row) {
+            $pattern = explode(' ', $row);
+            $this->sequence = explode(',', substr(str_repeat($pattern[1] . ',', 5), 0, -1));
+            $this->pattern = '.' . substr(str_repeat($pattern[0] . '?', 5), 0, -1) . '.';
+            $total = $this->checkPattern(0, 0, 0);
+//            echo $this->pattern . ' ' . implode(',',$this->sequence) ."::".$total."<br/>";
+            $subTotal += $total;
+        }
+
+        return $subTotal;
+    }
+}
